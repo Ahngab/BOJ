8805347b4343accff9a5a5ab035c3a74bc1169bd
Ahngab <102839564+Ahngab@users.noreply.github.com>
2022-11-14 13:15:20 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+
+    printf("%d \n", num1+num2);    // 변수의 내용을 출력
+
+    return 0;
+}
