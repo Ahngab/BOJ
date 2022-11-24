83cbde8ea493a9c357c71803a01211e9ed59dd4d
Ahngab <102839564+Ahngab@users.noreply.github.com>
2022-11-24 10:01:25 +0900

    [Silver II] Title: 나무 자르기, Time: 516 ms, Memory: 260064 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,19 @@
+import sys
+
+N, need = map(int, sys.stdin.readline().split())
+trees = list(map(int, sys.stdin.readline().split()))
+
+max_height = max(trees)
+min_height = 0
+
+while min_height <= max_height:
+    num = (max_height + min_height) // 2
+    cut = 0
+    for i in trees:
+        if i > num:
+            cut += i - num
+    if cut < need:
+        max_height = num - 1
+    elif cut >= need:
+        min_height = num + 1
+print(max_height)
