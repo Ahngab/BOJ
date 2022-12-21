5962358a2449e909d8315cd50412496c46d75673
Ahngab <102839564+Ahngab@users.noreply.github.com>
2022-12-21 23:48:41 +0900

    Update 에디터.py

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -2,17 +2,20 @@ import sys
 
 stack_l = list(input())
 stack_r = []
-n = int(input())
+N = int(input())
 
-for i in range(n):
+for i in range(N):
     command = sys.stdin.readline().split()
 
     if command[0] == "L" and stack_l:
         stack_r.append(stack_l.pop())
+        
     elif command[0] == "D" and stack_r:
         stack_l.append(stack_r.pop())
+        
     elif command[0] == "B" and stack_l:
         stack_l.pop()
+        
     elif command[0] == "P":
         stack_l.append(command[1])
 
